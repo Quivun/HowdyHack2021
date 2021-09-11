d44c94251b31eb35de968e6eefcc5e78d9ffc1b6
Quivun <Quivun@users.noreply.github.com>
2021-09-11 16:48:45 -0500

    organize and Done tutorial1
    Ready for clicky click, looking for pop up user interface next.

--- scripts/background.js
+++ scripts/background.js
@@ -10,11 +10,9 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 });
 
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "clicked_browser_action") {
-            var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-            console.log(firstHref);
-        }
+    function(request, sender, sendResponse) {
+      if( request.message === "open_new_tab" ) {
+        chrome.tabs.create({"url": request.url});
+      }
     }
-);
+  );

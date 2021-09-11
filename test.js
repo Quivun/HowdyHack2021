a45eb9b682c9d52098567a3efbd24bf34077a4ba
HarshGG <ritzred2012@gmail.com>
2021-09-11 16:53:13 -0500

    small irrelevant changes

---
+++ test.js
@@ -0,0 +1,12 @@
+// var jData = window.readFromFile();
+
+// var body = document.getElementById("body");
+var numVals = 7;
+var arrOfGPoints = Array.apply(null, Array(5)).map(function () {});
+var obj = {
+    table: []
+ };
+
+for(i = 0;i<numVals;i++){
+
+}
--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print("anything yall wanna experiment w/, do it here")
+# print("anything yall wanna experiment w/, do it here")
+import requests
+
+url = "https://youtube-to-mp4.p.rapidapi.com/url=&title"
+
+querystring = {"url":"https://www.youtube.com/watch?v=IfNB5RTxnhI","title":"Call of Duty : Modern Warfare 2 Remastered - All Weapon Reload Animations in 4 Minutes"}
+
+headers = {
+'x-rapidapi-host': "youtube-to-mp4.p.rapidapi.com",
+'x-rapidapi-key': "2c3b5a4a3cmsh6d69420296cde47p14f21djsn168c7a8ffa4c"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)

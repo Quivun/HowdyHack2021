bd79a68faae68e2056616ad9b915e6fa03f4e45f
HarshGG <ritzred2012@gmail.com>
2021-09-12 10:52:58 -0500

    scripts updated

--- scripts/RGBSCRIPT.js
+++ scripts/RGBSCRIPT.js
@@ -7,72 +7,112 @@ var arrOfGPoints = Array.apply(null, Array(5)).map(function () {});
 
 
 var object = {
-    angle: "90deg",
-    table: []
- };
- var point = {
-    "r":2,
-    "g":0,
-    "b":36,
-    "a":1,
-    "p":0
-}
-object.table.push(point)
+    "country": {
+        "r":121,
+        "g":44,
+        "b":9,
+        "a":68,
+        "p":0
+    },
+    "pop" : {
+        "r":226,
+        "g": 98,
+        "b": 220,
+        "a": 68,
+        "P": 22
+    },
+    "rock": {
+        "r":255,
+        "g":50,
+        "b":50,
+        "a":90,
+        "p":37
+    },
+    "hip-hop": {
+        "r":29,
+        "g":24,
+        "b":103,
+        "a":90,
+        "p":49
+    },
+    "edm" : {
+        "r":71,
+        "g":210,
+        "b":0,
+        "a":90,
+        "p":60
+    },
+    "classical" : {
+        "r":0,
+        "g":152,
+        "b":179,
+        "a":90,
+        "p":78
+    }
+    };
+//  var point = {
+//     "r":2,
+//     "g":0,
+//     "b":36,
+//     "a":1,
+//     "p":0
+// }
+// object.table.push(point)
 
-var point = {
-    "r":4,
-    "g":117,
-    "b":192,
-    "a":1,
-    "p":22
-}
-object.table.push(point)
+// var point = {
+//     "r":4,
+//     "g":117,
+//     "b":192,
+//     "a":1,
+//     "p":22
+// }
+// object.table.push(point)
 
-var point = {
-    "r":8,
-    "g":39,
-    "b":141,
-    "a":0,
-    "p":37
-}
-object.table.push(point)
+// var point = {
+//     "r":8,
+//     "g":39,
+//     "b":141,
+//     "a":0,
+//     "p":37
+// }
+// object.table.push(point)
 
-var point = {
-    "r":2,
-    "g":0,
-    "b":36,
-    "a":0,
-    "p":49
-}
-object.table.push(point)
+// var point = {
+//     "r":2,
+//     "g":0,
+//     "b":36,
+//     "a":0,
+//     "p":49
+// }
+// object.table.push(point)
 
-var point = {
-    "r":2,
-    "g":0,
-    "b":36,
-    "a":0,
-    "p":60
-}
-object.table.push(point)
+// var point = {
+//     "r":2,
+//     "g":0,
+//     "b":36,
+//     "a":0,
+//     "p":60
+// }
+// object.table.push(point)
 
-var point = {
-    "r":4,
-    "g":117,
-    "b":192,
-    "a":1,
-    "p":78
-}
-object.table.push(point)
+// var point = {
+//     "r":4,
+//     "g":117,
+//     "b":192,
+//     "a":1,
+//     "p":78
+// }
+// object.table.push(point)
 
-var point = {
-    "r":2,
-    "g":0,
-    "b":36,
-    "a":1,
-    "p":100
-}
+// var point = {
+//     "r":2,
+//     "g":0,
+//     "b":36,
+//     "a":1,
+//     "p":100
+// }
 
-object.table.push(point)
+// object.table.push(point)
 
 // readTextFile("C:\Users\ritzr\Desktop\cs_projects\selfdrive car\music_data.json", function(text){
 //     var data = JSON.parse(text);
@@ -83,43 +123,45 @@ object.table.push(point)
 // var prevPulse = 0
 // var pulseParser = 0
 var increment = 3
-
+var currGenre = "pop"
+var currVals = object[currGenre]
+console.log(currVals)
 setInterval(function(){updatePositions()}, 58.05)
 
 function updatePositions(){
     // var currPulse = pulse[pulseParser]
     // var brightener = currPulse/100
-    for(i = 0;i<object.table.length;i++){
-        currAttributes = object.table[i]
+    for(i = 0;i<currVals.length;i++){
+        currAttributes = currVals[i]
         currPos = currAttributes.p
         if(currPos+increment>100){
-            object.table[i].p=0
+            currVals[i].p=0
         }
         else
-            object.table[i].p+=increment
+            currVals[i].p+=increment
         // if(currPulse!=0){
-        //     object.table[i].r+=brightener
-        //     object.table[i].g+=brightener
-        //     object.table[i].b+=brightener
+        //     currVals[i].r+=brightener
+        //     currVals[i].g+=brightener
+        //     currVals[i].b+=brightener
         // }
         // else if(currPulse==0 && prevPulse!=0){
-        //     object.table[i].r-=brightener
-        //     object.table[i].g-=brightener
-        //     object.table[i].b-=brightener
+        //     currVals[i].r-=brightener
+        //     currVals[i].g-=brightener
+        //     currVals[i].b-=brightener
         //     brightener=0
         // }
         // prevPulse = currPulse
         // pulseParser++
     }
-    console.log(object.table)
-    setElements(object.table)
+    console.log(currVals)
+    setElements(currVals)
     return;
 }
 
-function setElements(table){
+function setElements(currVals){
     var styleText  = "background:linear-gradient(90deg,"
-    for(i = 0;i<table.length;i++){
-        var currAttributes = table[i]
+    for(i = 0;i<currVals.length;i++){
+        var currAttributes = currVals[i]
         var toAdd = " rgba(" + (currAttributes.r).toString() + "," + (currAttributes.g).toString() + "," +
         (currAttributes.b).toString() + "," + (currAttributes.a).toString() + ") " +
         (currAttributes.p).toString() + "%,"
@@ -130,8 +172,8 @@ function setElements(table){
     document.getElementById("top").style=styleText
     document.getElementById("bottom").style=styleText
     styleText  = "background:linear-gradient(180deg,"
-    for(i = 0;i<table.length;i++){
-        var currAttributes = table[i]
+    for(i = 0;i<currVals.length;i++){
+        var currAttributes = currVals[i]
         var toAdd = " rgba(" + (currAttributes.r).toString() + "," + (currAttributes.g).toString() + "," +
         (currAttributes.b).toString() + "," + (currAttributes.a).toString() + ") " +
         (currAttributes.p).toString() + "%,"

a1a53aad24fbd3dfacaecfa1dcd7006d0c628ce6
mzhang8132 <mzhang8132@gmail.com>
2021-09-12 03:00:39 -0500

    hmmsoajfiojoewj

--- pythonScripts/debug.py
+++ pythonScripts/debug.py
@@ -1,3 +1,8 @@
-import smooth
+import modify_ary
 import music_analysis
 
+twinklePitches = music_analysis.find_pitches("Twinkle-twinkle-Little-Star.wav")
+birthdayBeats = music_analysis.find_beats("happy-birthday.wav")
+
+
+print("hello world")
--- pythonScripts/modify_ary.py
+++ pythonScripts/modify_ary.py
@@ -21,7 +21,7 @@ def map_to_255(ary):
 def pulse(ary, sample_time, pulse_amplitude = 200, pulse_duration = 5): #pulse size and duration optional    .005805 is sample_time
     new_ary = np.zeros(len(ary))
     for t in ary:
-        sample_num = t/sample_time
+        sample_num = t/(sample_time + 20)
         for m in range(len(pulse_duration)):
             new_ary[sample_num + m] = pulse_amplitude/(m+1)
     return(new_ary)

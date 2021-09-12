5e4538d28b6f5556f00263508271041a21f11013
Quivun <Quivun@users.noreply.github.com>
2021-09-12 03:09:41 -0500

    SUCCESSFUL API

--- pythonScripts/apiJSxPY.py
+++ pythonScripts/apiJSxPY.py
@@ -1,16 +1,6 @@
 from flask import Flask, json, jsonify, request
 #from flask_restful import Resource, Api
 app = Flask(__name__)
-from youtubesearchpython import VideosSearch
-
-def video_link(Title):
-    videosSearch = VideosSearch(Title, limit = 1)
-    return(videosSearch.result()['result'][0]['link'])
-
-
-
-
-
 
 @app.route("/", methods=['GET','POST'])
 def index():
